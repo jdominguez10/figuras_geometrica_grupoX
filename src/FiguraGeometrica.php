a78e1acffccdfb0b286dca5555088d683c6046cc
Pablo <jdommay863@g.educaand.es>
2024-10-23 23:35:33 +0200

    corrección clase abstracta

--- src/FiguraGeometrica.php
+++ src/FiguraGeometrica.php
@@ -1,24 +1,16 @@
 <?php
-//Definición de la calse abstracta
-abstract class FiguraGeometrica(){
-  protected $nombre;
+// Clase abstracta FiguraGeometrica (ya definida previamente)
+abstract class FiguraGeometrica {
+    protected $nombre;
 
-  //Constructor para inicializar el nombre de la figura
-  public function __construct(string $nombre){
-    $this->nombre=$nombre;
-    
-  }
+    public function __construct(string $nombre) {
+        $this->nombre = $nombre;
+    }
 
-  //Método para obtener el nombre de la figura
-  public function getNombre(): string{
-    return $this->nombre;
-  }
+    public function getNombre(): string {
+        return $this->nombre;
+    }
 
-  //Métodos abstracto que deben ser implementados por las subclases
-  abstract public function area(): float;
-  abstract public function perimetro(): float;
-  
+    abstract public function area(): float;
+    abstract public function perimetro(): float;
 }
-
-
-?>
